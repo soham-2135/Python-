e2de04db00bc380389a7389b3a613bd3b917cab1
soham-2135 <pujarisoham270@gmail.com>
2025-02-02 16:51:29 +0530

    Update variables.py

--- Basics/Exercise/variables.py
+++ Basics/Exercise/variables.py
@@ -1,1 +1,15 @@
+#Exercise-1: create a variable named "break" and asiign it value 5, see what happens
+break=5
+
+# Exercise 2:
+"""birth_year= 2005
+current_year= 2025
+age=current_year - birth_year
+print("My age would be"+" "+str(age))"""
+
+# Exercise 3:
+First_name="Soham"
+Middle_name="Niren"
+Last_name="Pujari"
+print("My name is",First_name,Middle_name,Last_name)
 
